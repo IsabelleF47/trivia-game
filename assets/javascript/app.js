04363801d25a4df5a1eaa4ad3e68476f046719dd
Isabelle Farrell <IFarrell47@yahoo.com>
2019-07-08 17:01:03 -0400

    new additions and variables

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+
+
+// This is the counters for the game
+var wins= 0
+var loses= 0
+var timeCounter= (30 )
+
+// These are the questions that will be asked  to the user
+var questions ={
+    q1 :["What country does the Siamese cat variation originate from?"],
+    q2 :["what hair trype is more common in cats long or short hair ?"],
+    q3 :["Which pet do households have more of cats or dogs?"],
+    q4 :["What is the name of my first cat?"],
+
+
+};
