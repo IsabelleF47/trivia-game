897165219088bbbed6587f76d56716744ea92676
Isabelle Farrell <IFarrell47@yahoo.com>
2019-07-10 22:34:02 -0400

    new functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,33 +1,49 @@
+// This is the variables for the game 
+
+// var q1= document.game.q1.value;
+// var q2= document.game.q2.value;
+// var q3= document.game.q3.value;
+// var q4= document.game.q4.value;
+
+// var correct=0;
 
 
-// This is the counters for the game
-var wins= 0;
-var loses= 0;
-var counter= 0;
-var timeleft = 60;
 
-// These are the questions that will be asked  to the user
-var questions ={
-    q1 :["What country does the Siamese cat variation originate from?"],
-    q2 :["what hair trype is more common in cats long or short hair ?"],
-    q3 :["Which pet do households have more of cats or dogs?"],
-    q4 :["What is the name of my first cat?"],
+// This is where I created a function to store and check the users input
+
+function check() {
+    var q1= document.game.q1.value;
+    var q2= document.game.q2.value;
+    var q3= document.game.q3.value;
+    var q4= document.game.q4.value;
+
+    var correct=0;
+    if(q1=="thailand"){
+        correct++;
+    }
+    if(q2=="shorthair"){
+        correct++;
+    }
+    if(q3=="cats"){
+        correct++;
+    }
+    if(q3=="pepper"){
+        correct++;
+    }
+    document.getElementById("afterSubmit").style.visibility="visible";
+    document.getElementById("numberorrect").innerHTML="You got"+correct+"right.";
+    
+}
 
 
-};
+
+
+
+
+
+
+
+
 
 
 // this is the game timer funtion that will count down as the user guesses
-function timer () {
-    var timeCounter= select('#timer');
-    timer.html ('')
-    function timeIt () {
-
-        counter++;
-        timer.html(counter);
-
-    }
-    
-    setInterval (timeIt, 1000);
-
-}
